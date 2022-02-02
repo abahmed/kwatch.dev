1c1e3564db9430bd8cb3002623da4b704062aa08
Abdelrahman Ahmed <a.ahmed1026@gmail.com>
2022-02-02 04:55:41 +0200

    update google analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,8 +43,8 @@ const config = {
           changefreq: 'weekly',
           priority: 0.5,
         },
-        gtag: {
-          trackingID: '302070010',
+        googleAnalytics: {
+          trackingID: 'UA-162766515-2',
           anonymizeIP: true,
         },
       }),
